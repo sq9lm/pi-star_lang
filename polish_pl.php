cdd82de660910821bc8b7906dcb1ecf3afbf6159
Łukasz <54619104+sq9lm@users.noreply.github.com>
2019-12-03 00:14:00 +0100

    Add files via upload

---
+++ polish_pl.php
@@ -0,0 +1,145 @@
+<?php
+//
+// Polska PL Language Pack
+// Łukasz Misiura (SQ9LM)
+// Updated: 02-Dec-2019 v1.3
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Dźwięk cyfrowy",
+  "configuration"               =>  "Konfiguracja",
+  "dashboard_for"               =>  "Dashboard dla",
+  // Banner links
+  "dashboard"                   =>  "Dashboard",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Power",
+  "update"                      =>  "Aktualizacja",
+  "backup_restore"              =>  "Kopia/Przywracanie",
+  "factory_reset"               =>  "Przywracanie ustawień fabrycznych",
+  "live_logs"                   =>  "Logi na żywo :)",
+  // Config page section headdings
+  "hardware_info"               =>  "Informacje o sprzęcie bramki",
+  "control_software"            =>  "Oprogramowanie sterujące",
+  "mmdvmhost_config"            =>  "Konfiguracja MMDVMHost",
+  "general_config"              =>  "Konfiguracja ogólna",
+  "dmr_config"                  =>  "Konfiguracja DMR",
+  "dstar_config"                =>  "Konfiguracja D-Star",
+  "ysf_config"                  =>  "Konfiguracja YSF",
+  "p25_config"                  =>  "Konfiguracja P25",
+  "nxdn_config"                 =>  "Konfiguracja NXDN",
+  "pocsag_config"               =>  "Konfiguracja POCSAG",
+  "wifi_config"                 =>  "Konfiguracja Wireless",
+  "fw_config"                   =>  "Konfiguracja Zapory",
+  "remote_access_pw"            =>  "Hasło zdalnego Dostępu",
+  // Config Page - Section General
+  "setting"                     =>  "Ustawienia",
+  "value"                       =>  "Wartość",
+  "apply"                       =>  "Zapisz Zmiany",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Nazwa hosta",
+  "kernel"                      =>  "Kernel/Jądro",
+  "platform"                    =>  "Platforma",
+  "cpu_load"                    =>  "Obciążenie procesora",
+  "cpu_temp"                    =>  "Temp. procesora",
+  // Config Page - Control Software
+  "controller_software"         =>  "Oprogramowanie Sterujące",
+  "controller_mode"             =>  "Oprogramowanie kontrolera",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "Tryb DMR",
+  "d-star_mode"                 =>  "Tryb D-Star",
+  "ysf_mode"                    =>  "Tryb YSF",
+  "p25_mode"                    =>  "Tryb P25",
+  "nxdn_mode"                   =>  "Tryb NXDN",
+  "mmdvm_display"               =>  "Typ wyświetlania MMDVM",
+  "mode_hangtime"               =>  "Czas rozłączenia",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Znak wtwoławczy",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Częstotliwość Pracy",
+  "lattitude"                   =>  "Szer.geograficzna",
+  "longitude"                   =>  "Dł.geograficzna",
+  "town"                        =>  "Miasto",
+  "country"                     =>  "Kraj",
+  "url"                         =>  "Strona",
+  "radio_type"                  =>  "Tryb radia/modemu",
+  "node_type"                   =>  "Trym Pracy",
+  "timezone"                    =>  "Strefa czasowa",
+  "dash_lang"                   =>  "Język Dashboard'u",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "Sieć BrandMeister",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "Sieć DMR+",
+  "xlx_master"                  =>  "Wybór modułu XLX",
+  "xlx_enable"                  =>  "Włączenie XLX",
+  "dmr_cc"                      =>  "Kolor Kod DMR ",
+  "dmr_embeddedlconly"          =>  "Tylko wbudowany LC DMR",
+  "dmr_dumptadata"              =>  "Zrzut danych DMR",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "Znak wywoławczy RPT1",
+  "dstar_rpt2"                  =>  "Znak wywoławczy RPT2",
+  "dstar_irc_password"          =>  "Hasło zdalnego sostępu",
+  "dstar_default_ref"           =>  "Domyślny Reflektor",
+  "aprs_host"                   =>  "APRS Host",
+  "dstar_irc_lang"              =>  "Język ircDDBGateway",
+  "dstar_irc_time"              =>  "Time Announcements",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "Host startowy YSF",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "Host startowy P25",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "Host startowy NXDN",
+  "nxdn_ran"                     =>  "NXDN RAN",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Dostęp do Dashboard'u",
+  "fw_irc"                      =>  "Zdalny dostęp ircDDBGateway",
+  "fw_ssh"                      =>  "Dostęp SSH",
+  // Config Page - Password
+  "user"                        =>  "Nazwa Użytkownika",
+  "password"                    =>  "Hasło",
+  "set_password"                =>  "Ustaw Hasło",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Tryb włączony",
+  "net_status"                  =>  "Status Sieci",
+  "internet"                    =>  "Internet",
+  "radio_info"                  =>  "Informacja Radio",
+  "dstar_repeater"              =>  "D-Star Rypiter",
+  "dstar_net"                   =>  "Sieć D-Star",
+  "dmr_repeater"                =>  "DMR Rypiter",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "Sieć YSF",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "Sieć P25",
+  "nxdn_radio"                  =>  "NXDN Radio",
+  "nxdn_net"                    =>  "Sieć NXDN",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Godzina",
+  "mode"                        =>  "Tryb",
+  "callsign"                    =>  "Znak Wywoławczy",
+  "target"                      =>  "Cel",
+  "src"                         =>  "Żr.",        // Short version of "Source"
+  "dur"                         =>  "Cel",        // Short version of "Duration"
+  "loss"                        =>  "Strata",
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // POCSAG Specific
+  "pocsag_list"                 =>  "Aktywność bramy DAPNET",
+  "pocsag_timeslot"             =>  "Szczelina Czasowa",
+  "pocsag_msg"                  =>  "Wiadomość",
+  // Dashboard - Extra Info
+  "group"                       =>  "Grupa",
+  "logoff"                      =>  "Wyliguj!",
+  "info"                        =>  "Informacje",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Aktywność bramy",
+  "local_tx_list"               =>  "Lokalna aktywność RF",
+  "active_starnet_groups"       =>  "Aktywne grupy Starnet",
+  "active_starnet_members"      =>  "Aktywni członkowie grupy Starnet",
+  "d-star_link_manager"         =>  "Menedżer linków D-Star",
+  "d-star_link_status"          =>  "Informacje o łączu D-Star",
+  "service_status"              =>  "Status Usługi"
+);
+?>
